a11b6f7d98e285abd5635f561b7420900ad1da0a
rhilenova <rhilenova@gmail.com>
2013-10-06 09:29:16 -0400

    Updated version numbers.

--- common/rhilenova/afk/Reference.java
+++ common/rhilenova/afk/Reference.java
@@ -5,7 +5,7 @@ public class Reference
 	public static final String MOD_ID = "rn_afk";
 	public static final String MOD_NAME = "AFK";
 	public static final String MOD_CHANNEL = "rn_afk";
-	public static final String VERSION_NUMBER = "0.1";
+	public static final String VERSION_NUMBER = "0.2";
 	
 	public static final String CLIENT_PROXY_CLASS = "rhilenova.afk.proxy.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "rhilenova.afk.proxy.ServerProxy";
